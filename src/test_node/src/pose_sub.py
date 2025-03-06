02936ae48e6ece224d44e2200759b54ace14b108
RunzeZhou <125444672+RunzeZhou@users.noreply.github.com>
2025-03-06 21:39:40 +0800

    0306提交
    增加了可接受消息的订阅者

--- src/test_node/src/pose_sub.py
+++ src/test_node/src/pose_sub.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import rospy
-from geometry_msgs.msg import Pose
+from turtlesim.msg import Pose
 
 def poseCallback(msg):
     rospy.loginfo("turtle pose: x:%0.6f, y:%0.6f", msg.x, msg.y)
@@ -11,7 +11,7 @@ def pose_subscriber():
     # 初始化ROS节点
     rospy.init_node('pose_sub', anonymous=True)
     # 订阅名为/turtle1/pose的topic，消息类型为geometry_msgs::Pose，回调函数为poseCallback
-    rospy.Subscriber('turtle1/pose', Pose, poseCallback)
+    rospy.Subscriber('/turtle1/pose', Pose, poseCallback)
     # 循环等待回调函数
     rospy.spin()
 
